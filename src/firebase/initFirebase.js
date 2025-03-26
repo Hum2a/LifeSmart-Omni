7385bbfd6bd4505e52807f2a2d113a6f483b2bba
Humza Butt <Humzab1711@hotmail.com>
2025-03-26 14:20:56 +0000

    Refactor Firebase configuration to use environment variables for sensitive data. This change enhances security by preventing hardcoded credentials in the codebase.

--- src/firebase/initFirebase.js
+++ src/firebase/initFirebase.js
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBty3iHFAl_HrfBlIZlXp8JDGIOlkZ1gZE",
-  authDomain: "lifesmart-investing-tool.firebaseapp.com",
-  projectId: "lifesmart-investing-tool",
-  storageBucket: "lifesmart-investing-tool.appspot.com",
-  messagingSenderId: "949718346442",
-  appId: "1:949718346442:web:7c75842869f1a305c2e233",
-  measurementId: "G-E5MWPG1SGK",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
